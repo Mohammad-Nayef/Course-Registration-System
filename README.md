611c31766eb1d0bf264023997cd6d40e7b3e38af
mohammad-nayef <awladmohammad20@gmail.com>
2024-04-20 21:47:38 +0300

    Migrated models to MySQL

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Course-Registration-System
+# Course Registration System
+
+## Getting Started
+Download MySQL image: https://hub.docker.com/_/mysql<br>
+Run it with the environment variable MYSQL_ROOT_PASSWORD that has the password of the root user.<br>
+Execute this command in the container in order to run MySQL commands: 
+```sh
+mysql -u root -p
+```
+then enter the password which is set in the environment variables.<br>
+Execute: 
+```sh
+CREATE DATABASE reg;
+```
+and use the name `reg` for the client connection.<br>
+To start using the database and execute SQL queries, execute:
+```sh
+USE reg;
+```
---
+++ courses/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class CourseSchedule(models.Model):
+    id = models.IntegerField(primary_key=True, auto_created=True)
+    days = models.CharField(max_length=70)
+    startTime = models.TimeField()
+    endTime = models.TimeField()
+    roomNo = models.IntegerField()
+
+class Course(models.Model):
+    code = models.CharField(primary_key=True, max_length=20)
+    name = models.CharField(max_length=50, null=False)
+    description = models.CharField(max_length=200)
+    prerequisiteCode = models.CharField(models.ForeignKey('self', on_delete=models.SET_NULL), max_length=20)
+    instructor = models.CharField(max_length=50)
+    capacity = models.IntegerField()
+    scheduleId = models.IntegerField(models.ForeignKey(CourseSchedule, on_delete=models.SET_NULL))
+
+class Enrollment(models.Model):
+    studentId = models.IntegerField(models.ForeignKey(User, on_delete=models.CASCADE))
+    courseId = models.IntegerField(models.ForeignKey(Course, on_delete=models.CASCADE))
+
+    class Meta:
+        unique_together = ('studentId', 'courseId')
