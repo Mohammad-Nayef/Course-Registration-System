d0bb16761bf89ecf5d97212d5cd6ffd1973acdd0
mohammad-nayef <awladmohammad20@gmail.com>
2024-04-24 14:11:52 +0300

    Remove registeration data response

--- users/views.py
+++ users/views.py
@@ -1,5 +1,4 @@
 from rest_framework.decorators import api_view
-from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserSerializer
@@ -11,6 +10,6 @@ def register(request):
     
     if user_serializer.is_valid():
         user_serializer.save()
-        return Response(user_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_201_CREATED)
     
     return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
