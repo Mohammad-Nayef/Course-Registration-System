e7a1ac2f3f561fda90d68e736b58615fade14a46
mohammad-nayef <awladmohammad20@gmail.com>
2024-04-28 14:19:03 +0300

    Implement an API to register in a course

--- courses/urls.py
+++ courses/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('api/courses/<str:code>', views.get_course_by_code, name='get_course_details'),
     path('api/courses', views.search_courses, name='search_courses'),
+    path('api/registered-courses', views.register_course, name='register_course'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,6 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from .models import Course
+from .models import Course, Enrollment
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from django.db.models import Q, Count, F
@@ -49,3 +49,25 @@ def format_schedule(course):
     course.pop('schedule__days')
     course.pop('schedule__start_time')
     course.pop('schedule__end_time')
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def register_course(request):
+    course_code = request.data.get('course_code')
+    
+    try:
+        course = Course.objects.get(pk=course_code)
+
+    except Course.DoesNotExist:
+        return Response(
+            {'error': f'Course with code \'{course_code}\' is not found.'}, 
+            status=status.HTTP_404_NOT_FOUND)
+    
+    if Enrollment.objects.filter(student=request.user, course=course).exists():
+        return Response(
+            {'error': f'Course with code \'{course_code}\' is already registered.'}, 
+            status=status.HTTP_409_CONFLICT)
+    
+    Enrollment.objects.create(student=request.user, course=course)
+    return Response(status=status.HTTP_201_CREATED)
